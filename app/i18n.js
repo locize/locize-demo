1c010ee0cc9874747c25588fa45515a1510e717d
Jan Muehlemann <jan.muehlemann@gmail.com>
2016-06-08 12:39:04 +0200

    more flexibility

--- app/i18n.js
+++ app/i18n.js
@@ -1,19 +1,41 @@
 import i18n from 'locize';
 
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var backend = {
+  referenceLng: 'en',
+  apiKey: getParameterByName('api_key'),
+  projectId: getParameterByName('project_id')
+}
+if (getParameterByName('dev')) {
+  backend.loadPath = 'https://api-dev.locize.io/{{projectId}}/latest/{{lng}}/{{ns}}';
+  backend.addPath = 'https://api-dev.locize.io/missing/{{projectId}}/latest/{{lng}}/{{ns}}';
+}
 
 i18n
   .init({
     fallbackLng: 'en',
 
     // have a common namespace used around the full app
-    ns: ['common'],
-    defaultNS: 'common',
+    ns: ['app'],
+    defaultNS: 'app',
 
-    debug: true,
+    debug: false,
+    saveMissing: true,
 
     interpolation: {
       escapeValue: false // not needed for react!!
-    }
+    },
+
+    backend: backend
   });
 
 
