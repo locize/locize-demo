5c3d03a19136af2dc576632cab86441d01cbf716
Jan Muehlemann <jan.muehlemann@gmail.com>
2016-11-25 22:57:39 +0100

    prepare v2 demo

--- README.md
+++ README.md
@@ -20,4 +20,7 @@ Serve the root directory of this repository, you can use [http-server](https://g
 ```sh
 npm install http-server -g
 http-server -s -p 9000
+
+v2 url:
+http://localhost:9000/index_v2.html?project_slug=afk6scq0&name=Jan+Muehlemann&saveMissing=true&dev=true&project_id=046a3d29-e80f-4e68-bcbe-e1f5d1cd68fc&api_key=dfa10f40-960e-41b8-a614-25ee0405b59e
 ```
---
+++ shared.js
@@ -0,0 +1,63 @@
+// get param
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+// location change
+function updateQueryStringParameter(uri, key, value) {
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  }
+  else {
+    return uri + separator + key + "=" + value;
+  }
+}
+
+function changeLanguage(lng) {
+  window.location = updateQueryStringParameter(window.location.href, 'lng', lng);
+}
+
+// location change
+var ele = document.getElementById('languageSelect');
+var availableLngs = [];
+// create select options based on project languages
+locizify.getLanguages(function(err, lngs) {
+  availableLngs = Object.keys(lngs || {});
+  availableLngs.forEach(function(l) {
+    var lng = lngs[l];
+    if (lng.translated.production < 0.9) return;
+    var optEle = document.createElement("OPTION");
+    optEle.setAttribute('value', l);
+    optEle.innerHTML = lng.nativeName;
+    ele.appendChild(optEle);
+  });
+  updateSelect();
+});
+function updateSelect() {
+  var selected;
+  locizify.i18next.languages.forEach(function(l) {
+    if (!selected && availableLngs.indexOf(l) > -1) selected = l;
+  });
+  ele.value = selected || 'en';
+}
+locizify.i18next.on('languageChanged', function(lng) {
+  updateSelect();
+});
+function handleSelectChange() {
+  var value = ele.options[ele.selectedIndex].value;
+  changeLanguage(value);
+}
+
+window.locizeUtils = {
+  getParameterByName: getParameterByName,
+  updateQueryStringParameter: updateQueryStringParameter,
+  changeLanguage: changeLanguage
+};
